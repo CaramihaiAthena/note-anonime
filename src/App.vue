9e5f463f078784f4e22b1a0926412781d452395d
BojoagaMariaAndreea <114498184+BojoagaMariaAndreea@users.noreply.github.com>
2023-01-15 16:56:10 +0200

    Fixed bootstrap elements

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,240 @@
 <template>
-  <div id="app">
-    <Navbar @set-new-page="setNewPage" />
-    <!-- if we are on the home page, we create a form where we enter the data for the project -->
-    <div v-if="isHomePage">  
-      <div class="form-group">
-        <label for="fname">First Name</label>
-        <input type="text" v-model="fName" class="form-control" id="fname" placeholder="Enter First Name">
+  <div
+    class="bg-image"
+    style="
+      background-repeat: repeat;
+      background-attachment: fixed;
+      background-image: url('https://i.pinimg.com/originals/a4/17/cf/a417cf4d9dea17fd59a132333446ee7d.jpg');
+    "
+  >
+    <div
+      id="app"
+      class="card.mx-auto"
+      style="
+        width: 80%;
+        height: 100%;
+        margin-left: auto;
+        margin-right: auto;
+        padding-top: 5%;
+        padding-bottom: 10%;
+        border: none;
+      "
+    >
+      <Navbar @set-new-page="setNewPage" />
+      <!-- if we are on the home page, we create a form where we enter the data for the project -->
+      <div v-if="isHomePage">
+        <div
+          class="card.mx-auto"
+          style="
+            width: 80%;
+            height: 100%;
+            margin-left: auto;
+            margin-right: auto;
+            padding-top: 2%;
+            padding-bottom: 10%;
+            border: none;
+          "
+        >
+          <div
+            class="form-group bg-info text-light"
+            style="border-radius: 10px; padding: 1%"
+          >
+            <label
+              class="text-center"
+              style="
+                width: 100%;
+                margin-left: auto;
+                margin-right: auto;
+                padding-top: 0.5%;
+                padding-bottom: 0.5%;
+                border: none;
+              "
+              for="fname"
+              >First Name</label
+            >
+            <input
+              type="text"
+              v-model="fName"
+              class="form-control"
+              id="fname"
+              placeholder="Enter First Name"
+            />
+          </div>
+          <div
+            class="form-group bg-info text-light"
+            style="border-radius: 10px; padding: 1%"
+          >
+            <label
+              class="text-center"
+              style="
+                width: 100%;
+                margin-left: auto;
+                margin-right: auto;
+                padding-top: 0.5%;
+                padding-bottom: 0.5%;
+                border: none;
+              "
+              for="lname"
+              >Last Name</label
+            >
+            <input
+              type="text"
+              v-model="lName"
+              class="form-control"
+              id="lname"
+              placeholder="Last Name"
+            />
+          </div>
+          <div
+            class="form-group bg-info text-light"
+            style="border-radius: 10px; padding: 1%"
+          >
+            <label
+              class="text-center"
+              style="
+                width: 100%;
+                margin-left: auto;
+                margin-right: auto;
+                padding-top: 0.5%;
+                padding-bottom: 0.5%;
+                border: none;
+              "
+              for="title"
+              >Project Title</label
+            >
+            <input
+              type="text"
+              v-model="title"
+              class="form-control"
+              id="title"
+              placeholder="Project Title"
+            />
+          </div>
+          <div
+            class="form-group bg-info text-light"
+            style="border-radius: 10px; padding: 1%"
+          >
+            <label
+              class="text-center"
+              style="
+                width: 100%;
+                margin-left: auto;
+                margin-right: auto;
+                padding-top: 0.5%;
+                padding-bottom: 0.5%;
+                border: none;
+              "
+              for="project"
+              >Project</label
+            >
+            <textarea
+              type="text"
+              v-model="projectText"
+              class="form-control w-100 h-100"
+              id="project"
+              placeholder="Write the project here"
+            ></textarea
+            ><br />
+            <div class="custom-file">
+              <input
+                type="file"
+                class="custom-file-input"
+                id="ctrl"
+                webkitdirectory
+                directory
+                multiple
+              />
+              <label class="custom-file-label" for="ctrl">Choose file/files</label>
+            </div>
+            <!-- <div class="custom-file"> -->
+            <!-- <input type="file" id="project" name="project" multiple><br><br> -->
+            <!-- <input type="file" id="ctrl" webkitdirectory directory multiple /> -->
+            <!-- </div> -->
+          </div>
+          <!-- if we click submit button, we add the our project-->
+          <div class="d-flex justify-content-center">
+            <button
+              type="submit"
+              class="btn btn-primary bg-info text-light"
+              @click="submitHandler"
+              style="width: 40%; margin-top: 5% border-radius: 15px; border: none;"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
       </div>
-      <div class="form-group">
-        <label for="lname">Last Name</label>
-        <input type="text" v-model="lName" class="form-control" id="lname" placeholder="Last Name">
+      <div v-else-if="projects.length" v-for="(project, index) in projects" :key="index">
+        <div
+          class="card.mx-auto"
+          style="
+            width: 80%;
+            height: 100%;
+            margin-top: 2%;
+            margin-left: auto;
+            margin-right: auto;
+            border: none;
+          "
+        >
+          <GradingCard
+            :project="project"
+            :index="index"
+            @project-graded="handleProjectGraded"
+          />
+        </div>
       </div>
-      <div class="form-group">
-        <label for="title">Project Title</label>
-        <input type="text" v-model="title" class="form-control" id="title" placeholder="Project Title">
+      <div v-else>
+        <!-- if we didn't submit any project, this message will appear -->
+        <div
+          class="card.mx-auto"
+          style="
+            width: 80%;
+            height: 100%;
+            padding-top: 15%;
+            padding-bottom: 50%;
+            margin-left: auto;
+            margin-right: auto;
+            border: none;
+          "
+        >
+          <h1>
+            <center>No projects to grade</center>
+          </h1>
+        </div>
       </div>
-      <div class="form-group">
-        <label for="project">Project</label>
-        <textarea type="text" v-model="projectText" class="form-control w-100 h-100" id="project" placeholder="Write the project here"></textarea><br>
-        <!-- <input type="file" id="project" name="project" multiple><br><br> -->
-        <input type="file" id="ctrl" webkitdirectory directory multiple/>
-      </div>
-      <!-- if we click submit button, we add the our project-->
-      <button type="submit" class="btn btn-primary" @click="submitHandler">Submit</button>
     </div>
-    <div v-else-if="projects.length" v-for="(project, index) in projects" :key="index">
-      <GradingCard
-        :project="project"
-        :index="index"
-        @project-graded="handleProjectGraded"
-      />
-    </div >
-    <div v-else>
-      <!-- if we didn't submit any project, this message will appear -->
-      <h1>No projects to grade</h1>
-    </div >
   </div>
 </template>
 
 <script>
-// we imported the files 
-import { submitProject } from "../sdk/api"
-import GradingCard from './components/GradingCard.vue'
-import Navbar from "./components/Navbar.vue"
+// we imported the files
+import { submitProject } from "../sdk/api";
+import GradingCard from "./components/GradingCard.vue";
+import Navbar from "./components/Navbar.vue";
 
 export default {
   components: {
     Navbar,
     GradingCard,
   },
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
     return {
       //initially, our data are empty
-      fName: '',
-      lName: '',
-      title: '',
-      projectText: '',
-      currentPage: 'home',
-      projects: []
-    }
+      fName: "",
+      lName: "",
+      title: "",
+      projectText: "",
+      currentPage: "home",
+      projects: [],
+    };
   },
   // computed properties are cached based on their reactive dependencies (will only re-evaluate when some of its reactive dependencies have changed)
   computed: {
     isHomePage() {
       // a computed getter
-      return this.currentPage === 'home'
-    }
+      return this.currentPage === "home";
+    },
   },
   methods: {
     async submitHandler() {
@@ -75,23 +243,23 @@ export default {
         lName: this.lName,
         title: this.title,
         projectText: this.projectText,
-      }
-      await submitProject(project)
+      };
+      await submitProject(project);
       //we push our project into our array of proojects
-      this.projects.push(project)
+      this.projects.push(project);
     },
     //setter
-    setNewPage(page){
-      this.currentPage = page
+    setNewPage(page) {
+      this.currentPage = page;
     },
     //The code above parses the JSON object and assigns the grades to each project.
-    handleProjectGraded(params){
-      this.projects[params.index].grades.push(params.grade)
-      //creates a JSON object and assigns the values of this new object to the projects list 
-      //so that they can be parsed later on into individual objects with their respective grade 
+    handleProjectGraded(params) {
+      this.projects[params.index].grades.push(params.grade);
+      //creates a JSON object and assigns the values of this new object to the projects list
+      //so that they can be parsed later on into individual objects with their respective grade
       //information stored inside them
-      this.projects = JSON.parse(JSON.stringify(this.projects))
-    }
+      this.projects = JSON.parse(JSON.stringify(this.projects));
+    },
   },
-}
+};
 </script>
